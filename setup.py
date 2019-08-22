9088c3b416b857ac0a66b62afdc23ca467c2ff18
gmorello <giuseppe.morello.11@ucl.ac.uk>
2019-08-22 09:12:07 +0000

    setup for new version

---
+++ setup.py
@@ -0,0 +1,66 @@
+from setuptools import setup
+import codecs
+import os
+import glob
+
+name = 'exotethys'
+description = ''
+url = ''
+install_requires = ['matplotlib', 'docopt', 'astropy', 'scipy', 'ephem']
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+subdirs_to_include = []
+for x in os.walk(name):
+    if os.path.isdir(x[0]):
+        if x[0] != name:
+            subdirs_to_include.append(x[0])
+
+files_to_include = []
+for x in glob.glob(os.path.join(name, '*')):
+    if os.path.isfile(x):
+        if x.split('.')[-1] not in ['py']:
+            files_to_include.append(os.path.join(name, os.path.split(x)[1]))
+
+files_to_include.append('README.md')
+files_to_include.append('LICENSE')
+
+w = open('MANIFEST.in', 'w')
+for i in subdirs_to_include:
+    w.write('include ' + os.path.join(i, '*') + ' \n')
+
+for i in files_to_include:
+    w.write('include ' + i + ' \n')
+
+w.close()
+
+with codecs.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+version = ' '
+for i in open(os.path.join(name, '__init__.py')):
+    if len(i.split('__version__')) > 1:
+        version = i.split()[-1][1:-1]
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=long_description,
+    url=url,
+    author='Giuseppe Morello',
+    author_email='giuseppe.morello.11@ucl.ac.uk',
+    license='Creative Commons Attribution 4.0 International License',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering :: Astronomy',
+                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Programming Language :: Python :: 3.7',
+                 ],
+    packages=[name],
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+)
