ae5537b2fade93b082be720100b9a85836c721d9
gmorello <giuseppe.morello.11@ucl.ac.uk>
2019-10-08 14:05:20 +0200

    update databases

--- exotethys/_1databases.py
+++ exotethys/_1databases.py
@@ -89,6 +89,6 @@ class Database:
 
 
 databases = {
-"Phoenix_2012_13":Database('Phoenix_2012_13', date_to_update='190208', vital=True),
-"Phoenix_2018":Database("Phoenix_2018", date_to_update='190208', vital=True)
+"Phoenix_2012_13":Database('Phoenix_2012_13', date_to_update='190116’, vital=True),
+"Phoenix_2018":Database('Phoenix_2018', date_to_update='190116’, vital=True), "Atlas_2000":Database('Atlas_2000', date_to_update='190929', vital=True)
 }
