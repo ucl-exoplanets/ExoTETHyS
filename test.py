2eb6689b163387624b76cd292f8b5b3d844cb355
gmorello <giuseppe.morello.11@ucl.ac.uk>
2019-10-30 15:25:18 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+from exotethys import sail, trip
+
+sail.ldc_calculate('tests/sail_example1.txt')
+#sail.ldc_calculate('tests/sail_example2.txt')
+#sail.ldc_calculate('tests/sail_example3.txt')
+#sail.ldc_calculate('tests/sail_example4.txt')
+#sail.ldc_calculate('tests/sail_example5.txt')
+#sail.ldc_calculate('tests/sail_example6.txt')
+#sail.ldc_calculate('tests/sail_example7.txt')
+#trip.trip_calculate('tests/trip_example.txt')
