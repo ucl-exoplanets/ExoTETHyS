339a03c915e4e05978437ead5f4796ac2a8cefaf
gmorello <giuseppe.morello.11@ucl.ac.uk>
2019-12-15 08:51:20 +0100

    Update __init__.py
    version 1.0.2

--- exotethys/__init__.py
+++ exotethys/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from .sail import *
 from .trip import *
