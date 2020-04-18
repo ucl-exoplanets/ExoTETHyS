58da149fa6e8a5d1d8602f41a25787a70ca54268
gmorello <giuseppe.morello.11@ucl.ac.uk>
2020-04-18 16:51:05 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__version__ = '2.x.x'
+
+from .sail import *
+from .trip import *
+from .boats import *
+from .manage_database import *
