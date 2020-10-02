bf1f8779355670bad06948ebf8f746e3e7febd63
gmorello <giuseppe.morello.11@ucl.ac.uk>
2020-10-02 15:15:13 +0200

    Add files via upload
    Adding Stagger_2015

--- exotethys/_database.py
+++ exotethys/_database.py
@@ -111,5 +111,5 @@ class Database:
 
 databases = {
 "Phoenix_2012_13":Database('Phoenix_2012_13', date_to_update='200412', vital=True),
-"Phoenix_2018":Database('Phoenix_2018', date_to_update='200412', vital=True), "Atlas_2000":Database('Atlas_2000', date_to_update='200403', vital=True)
+"Phoenix_2018":Database('Phoenix_2018', date_to_update='200412', vital=True), "Atlas_2000":Database('Atlas_2000', date_to_update='200403', vital=True), "Stagger_2015":Database('Stagger_2015', date_to_update='201001', vital=True)
 }
