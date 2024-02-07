8890fbaa3b977513c85f50fd75c00ceb79fba5fa
gmorello <giuseppe.morello.11@ucl.ac.uk>
2024-02-07 09:52:11 +0100

    Update __init__.py
    new subsubversion

--- exotethys/__init__.py
+++ exotethys/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '2.0.10'
+__version__ = '2.0.11'
 
 from .sail import *
 from .trip import *
