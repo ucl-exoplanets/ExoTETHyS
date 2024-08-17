69c08ce487440b076b56e656743a8c128c126002
gmorello <giuseppe.morello.11@ucl.ac.uk>
2024-08-16 23:43:11 -0400

    Update __init__.py
    2.0.13

--- exotethys/__init__.py
+++ exotethys/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = '2.0.12'
+__version__ = '2.0.13'
 
 from .sail import *
 from .trip import *
